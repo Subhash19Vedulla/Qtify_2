56d0c6a7d489f42baeba06ea08bcc58302bef601
Subhash19Vedulla <83652945+Subhash19Vedulla@users.noreply.github.com>
2026-02-27 19:56:14 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,18 +5,27 @@ import SearchBar from "../SearchBar/SearchBar";
 import Button from "../Button/Button";
 
 const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      {/* Logo */}
+      <div className={styles.logoWrapper}>
+        <Logo data-testid="navbar-logo" />
+      </div>
 
-    return (
-        <>
-            <nav className={styles.navbar}>
-                <Logo />
-                <SearchBar search={"Search a song of your choice"} />
-                <Button children={"Give Feedback"} />
-            </nav>
+      {/* Search bar */}
+      <div className={styles.searchWrapper}>
+        <SearchBar
+          search="Search a song of your choice"
+          data-testid="navbar-search"
+        />
+      </div>
 
-        </>
-
-    );
-}
+      {/* Button */}
+      <div className={styles.buttonWrapper}>
+        <Button data-testid="navbar-button">Give Feedback</Button>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
