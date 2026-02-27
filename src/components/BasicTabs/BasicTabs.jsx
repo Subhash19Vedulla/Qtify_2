5350622e41316879c70f552860bc3624667d835e
Subhash19Vedulla <83652945+Subhash19Vedulla@users.noreply.github.com>
2026-02-27 19:18:26 +0530

    Update BasicTabs.jsx

--- src/components/BasicTabs/BasicTabs.jsx
+++ src/components/BasicTabs/BasicTabs.jsx
@@ -1,38 +1,8 @@
 import * as React from 'react';
 import { useState } from 'react';
-import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import styles from "./BasicTabs.module.css";
-
-//-----no need to show custom panels---------
-// function CustomTabPanel(props) {
-//   const { children, value, index, ...other } = props;
-
-//   return (
-//     <div
-//       role="tabpanel"
-//       hidden={value !== index}
-//       id={`simple-tabpanel-${index}`}
-//       aria-labelledby={`simple-tab-${index}`}
-//       {...other}
-//     >
-//       {value === index && (
-//         <Box sx={{ p: 3 }}>
-//           <Typography>{children}</Typography>
-//         </Box>
-//       )}
-//     </div>
-//   );
-// }
-
-// CustomTabPanel.propTypes = {
-//   children: PropTypes.node,
-//   index: PropTypes.number.isRequired,
-//   value: PropTypes.number.isRequired,
-// };
 
 function a11yProps(index) {
   return {
@@ -41,24 +11,27 @@ function a11yProps(index) {
   };
 }
 
-export default function BasicTabs({ handleChangeIndex}) {
+export default function BasicTabs({ handleChangeIndex }) {
   const [value, setValue] = useState(0);
 
   const handleChange = (event, newValue) => {
-    //setting the "value" state of this component so that the position of active tab and its indicator can change
     setValue(newValue);
-    //to pass the index of freshly selected tab to app.js
     handleChangeIndex(newValue);
   };
 
-// All rock pop jezz blues
   return (
     <Box sx={{ width: '100%' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs scrollButtons="auto" variant="scrollable" allowScrollButtonsMobile value={value} onChange={handleChange} aria-label="basic tabs example" 
-         TabIndicatorProps={{
-            style: { backgroundColor: "#34c94b" }
-        }} textColor='#34c94b' >
+        <Tabs
+          scrollButtons="auto"
+          variant="scrollable"
+          allowScrollButtonsMobile
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+          TabIndicatorProps={{ style: { backgroundColor: "#34c94b" } }}
+          textColor="#34c94b"
+        >
           <Tab label="All" {...a11yProps(0)} />
           <Tab label="Rock" {...a11yProps(1)} />
           <Tab label="Pop" {...a11yProps(2)} />
@@ -66,15 +39,6 @@ export default function BasicTabs({ handleChangeIndex}) {
           <Tab label="Blues" {...a11yProps(4)} />
         </Tabs>
       </Box>
-      {/* <CustomTabPanel value={value} index={0}></CustomTabPanel>
-      <CustomTabPanel value={value} index={1}></CustomTabPanel>
-      <CustomTabPanel value={value} index={2}></CustomTabPanel>
-      <CustomTabPanel value={value} index={3}></CustomTabPanel>
-      <CustomTabPanel value={value} index={4}></CustomTabPanel> */}
     </Box>
   );
 }
-
-
-// git config --global --unset user.name
-// git config --global --unset user.email
